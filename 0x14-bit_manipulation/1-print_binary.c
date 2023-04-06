7c9050fdb1658ec68f9f91bb119fecc14abc54b3
Salsaadany <s.a.alsaadany@gmail.com>
2023-04-06 23:41:23 +0200

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_binary - converts a decimal number to a base2 num
+* @n: the num to be printed in base2 numerical system
+*/
+void print_binary(unsigned long int n)
+{
+int cnt = 0, x;
+unsigned long int crrnt;
+
+for (x = 63; x >= 0; x--)
+{
+crrnt = n >> x;
+if (crrnt & 1)
+{
+_putchar('1');
+cnt++;
+}
+else if (cnt)
+_putchar('0');
+}
+if (!cnt)
+_putchar('0');
+}
