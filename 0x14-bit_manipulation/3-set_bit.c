54a03477d2a51b43f619bee7fbcc5ad38e1cbc00
Salsaadany <s.a.alsaadany@gmail.com>
2023-04-07 23:03:53 +0200

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,14 +3,14 @@
  * set_bit - the objective is to sit the bit's index to 1
  * @n: pointer of the wannabe changed num
  * @index: the bit index is to be set to 1
- *
  * Return: 1 if successful, -1 if not
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int x = 0;
-	if (index <63)
-        {
+
+	if (index < 63)
+	{
 		x = 1 << index;
 		*n = (*n | x);
 		return (1);
