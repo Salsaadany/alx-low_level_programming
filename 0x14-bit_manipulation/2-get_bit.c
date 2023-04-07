41ae200f75ce53d7e7df8a6aff2f36736041e01e
Salsaadany <s.a.alsaadany@gmail.com>
2023-04-07 22:29:25 +0200

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,16 @@
 * get_bit - the objective is returning bit value in a base10 num
 * @n: search of a number
 * @index: the bit index
+*
 * Return: the bit value 
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 int bvalue = 0;
-if (index > 63)
-return (-1);
-bvalue = (n >> index) & 1;
+if (index < 63)
+bvalue = n >> index;
+bvalue = bvalue & 1;
 return (bvalue);
+else
+return (-1);
 }
