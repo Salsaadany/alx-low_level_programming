9e0cd318690e80d7670abce3bb34081f44da5ece
Salsaadany <s.a.alsaadany@gmail.com>
2023-04-08 21:50:41 +0200

    Update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include<math.h>
 /**
 * print_binary - converts a decimal number to a base2 num
 * @n: the num to be printed in base2 numerical system
 */
 void print_binary(unsigned long int n)
 {
-int cnt = 0, x;
-unsigned long int crrnt;
+	int cnt = 0, x;
+	unsigned long int bin, temp;
 
-for (x = 63; x >= 0; x--)
-{
+	if (n == 0)
+		return 0; 
 
-/*crrnt = n >> x;*/
+	for (x = 63; x >= 0; x--)
+	{
+		temp = n >> x;
 
-crrnt = n / pow (n,x);
-if (crrnt & 1)
-{
-_putchar('1');
-cnt++;
-}
-else if (cnt)
-_putchar('0');
-}
-if (!cnt)
-_putchar('0');
+		if (temp & 1)
+		{
+			_putchar('1');
+			cnt++;
+		}
+		else if (cnt)
+			_putchar('0');
+	}
+	if (!cnt)
+		_putchar('0');
 }
